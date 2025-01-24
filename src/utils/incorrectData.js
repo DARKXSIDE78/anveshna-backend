ee258d3186f78ebe52d368c86711bb0cd6bb9f29
DARKXSIDE78 <nk1010057@gmail.com>
2025-01-24 09:30:10 +0530

    Update incorrectData.js

--- src/utils/incorrectData.js
+++ src/utils/incorrectData.js
@@ -4,7 +4,7 @@ export const incorrectDataID = () => [
     idGogo: "tokidoki-bosotto-russia-go-de-dereru-tonari-no-alya-san",
     idGogoDub: "tokidoki-bosotto-russia-go-de-dereru-tonari-no-alya-san-dub",
     idZoro: "alya-sometimes-hides-her-feelings-in-russian-19254",
-    id9anime: "tokidoki-bosotto-russiago-de-dereru-tonari-no-alya-san.llxvz",
-    idPahe: "",
+    id9anime: "alya-sometimes-hides-her-feelings-in-russian-19254",
+    idPahe: "08733ca0-75f0-40d2-8988-bbfe670491db",
   },
 ];
